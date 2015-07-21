5952441c575aa207c1e589bafc96f768b22b4535
murderface <murzak.vv@gmail.com>
2015-07-21 15:06:48 +0300

    initial commit, really

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+STI::Application.config.secret_token = '327678a1fc0dc0baa5cd7d085bb74c12450116feadf1e439e8c5ce111e60eeddd0105f60d7bb03db35da670159f5efab25b710d66d0874cb688a5ffca04b1a3f'
